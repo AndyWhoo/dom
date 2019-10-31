b969993cdde40ff9564b5dfabc7a64b1843ddd2a
Andy Wu <33040315+AndyWhoo@users.noreply.github.com>
2019-10-31 10:21:08 -0700

    kiwi sound

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
 let kiwiButton;
 let kiwiBird;
 let cnv;
+let kiwiCall;
+
+function preload() {
+  soundFormats('mp3', 'ogg');
+  kiwiCall = loadSound('kiwicall.wav');
+}
+
 
 function setup() {
   cnv = createCanvas(128,126);
@@ -8,8 +15,17 @@ function setup() {
   kiwiButton= select('#kiwiButton')
   kiwiButton.style("padding","50px");
   kiwiButton.style("background-color","#7affa0");
+
+kiwiButton.mousePressed(toggleKiwi);
+
 }
 
 function draw() {
   background(220);
+}
+
+function toggleKiwi(){
+kiwiCall.setVolume(0.7);
+kiwiCall.play();
+
 }
