2fa097f7d3f9326d66400a969072b2cb70c1d098
Andy Wu <33040315+AndyWhoo@users.noreply.github.com>
2019-10-31 09:37:00 -0700

    button

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ function setup() {
   noCanvas();
   kiwiButton= select('#kiwiButton')
   kiwiButton.style("padding","50px");
+  kiwiButton.style("background-color","#7affa0");
 }
 
 function draw() {
